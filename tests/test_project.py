727039f06c000ebb5be42414fe3f7d3124dd8310
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-07-09 15:26:40 +0900

    add unit-tets for path and delete handles

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,16 @@
+import os
 import pytest
 
 URL = "api/projects/"
+EXPECTED_KEYS = {
+    'name',
+    'description',
+    'full_amount',
+    'id',
+    'invested_amount',
+    'fully_invested',
+    'created_date',
+}
 
 
 @pytest.mark.parametrize(
@@ -93,16 +103,7 @@ async def test_create_project(async_client):
         response.status_code == 201
     ), "Создание проекта с корректными данными не должно вызывать ошибок."
     data = response.json()
-    expected_keys = {
-        "name",
-        "description",
-        "full_amount",
-        "id",
-        "invested_amount",
-        "fully_invested",
-        "created_date",
-    }
-    missing_keys = expected_keys - data.keys()
+    missing_keys = EXPECTED_KEYS - data.keys()
     assert not missing_keys, (
         f"При коректном запросе на создание проекта к {URL} в ответе не"
         f"хватает следующих ключей: `{"`, `".join(missing_keys)}`"
@@ -150,3 +151,141 @@ async def test_get_all_projects(async_client):
             "invested_amount": 0,
         },
     ], f"GET-запрос к {URL} должен вернуть список существующих проектов."
+
+
+@pytest.mark.parametrize(
+    "json_data",
+    [
+        {
+            "name": "Test name change", "description": "Test description change", "full_amount": 2000,
+        }
+    ]
+)
+async def test_update_project_with_non_exist_id(async_client, json_data):
+    response = await async_client.patch(os.path.join(URL, "100"), json=json_data,)
+    assert response.status_code == 404, (
+        "Запрос с попыткой обновления проекта с несуществующим id должен"
+        " возвращать ошибку со статусом 404"
+    )
+
+
+@pytest.mark.usefixtures("charity_project_first")
+@pytest.mark.parametrize(
+    "json_data, expected_data",
+    [
+        (
+            {"full_amount": 2000},
+            {
+                "id": 1,
+                "name": "Test Charity Project number 1",
+                "description": "This is a test charity project number 1",
+                "full_amount": 2000,
+                "created_date": "2023-01-01T00:00:00",
+                "fully_invested": False,
+                "invested_amount": 0,
+            },
+        ),
+        (
+            {"name": "Testing change project name"},
+            {
+                "id": 1,
+                "name": "Testing change project name",
+                "description": "This is a test charity project number 1",
+                "full_amount": 1000,
+                "created_date": "2023-01-01T00:00:00",
+                "fully_invested": False,
+                "invested_amount": 0,
+            },
+        ),
+        (
+            {"description": "Testing change project description"},
+            {
+                "id": 1,
+                "name": "Test Charity Project number 1",
+                "description": "Testing change project description",
+                "full_amount": 1000,
+                "created_date": "2023-01-01T00:00:00",
+                "fully_invested": False,
+                "invested_amount": 0,
+            },
+        ),
+    ]
+)
+async def test_update_project(async_client, charity_project_first, json_data, expected_data,):
+    url = os.path.join(URL, "1")
+    response = await async_client.patch(url, json=json_data)
+    assert response.status_code == 200, (
+        f"Коректный PATCH-запрос к {url} должен вернуть статус-код 200"
+    )
+    response_data = response.json()
+    missing_keys = EXPECTED_KEYS - response_data.keys()
+    assert not missing_keys, (
+        f'В ответе на PATCH-запрос к {url} не хвататет следующих ключей:'
+        f'`{"`, `".join(missing_keys)}`"'
+    )
+    response_data.pop("close_date", None)
+    assert response_data == expected_data, (
+        f"Структура ответа на PATCH-запрос к {url} не соответствует"
+        " ожидаемому."
+    )
+
+
+@pytest.mark.usefixtures("charity_project_first")
+@pytest.mark.parametrize(
+    "json_data",
+    [
+        {"name": ""},
+        {"description": ""},
+        {"full_amount": ""},
+        {"full_amount": "123abc"},
+        {"full_amount": 0.5},
+        {"full_amount": 0},
+        {"full_amount": -1},
+    ],
+)
+def test_update_project_with_invalid_data(test_client, json_data):
+    url = os.path.join(URL, "1")
+    response = test_client.patch(url, json=json_data)
+    assert response.status_code == 422, (
+        f"Убедитесь что при попытке отправить PATCH-запрос к {url} с"
+        "некоректыми данными: постые поля name, description, full_amount"
+        "текстовые значения или отрицательные числа или числа с плавоющей"
+        "точкой возаращется ответ со статус-кодом 422"
+    )
+
+
+@pytest.mark.usefixtures("charity_project_first")
+@pytest.mark.parametrize(
+    "json_data",
+    [
+        {"invested_amount": 500},
+        {"fully_invested": True},
+        {"close_date": "2023-01-01T00:00:00"},
+        {"created_date": "2023-01-01T00:00:00"}
+    ],
+)
+def test_update_project_with_default_filling_fields(test_client, json_data):
+    url = os.path.join(URL, "1")
+    response = test_client.patch(url, json=json_data)
+    assert response.status_code == 422, (
+        f"Убедитесь что при попытке отправить PATCH-запрос к {url} с"
+        " полями не предусмотренными спецификацией API для этого эндпоинта"
+        "возвращает статус код 422"
+    )
+
+
+async def test_delete_project_with_non_exist_id(async_client, charity_project_first):
+    url = os.path.join(URL, "100")
+    response = await async_client.delete(url)
+    assert response.status_code == 404, (
+        f"DELETE-запрос к {url} должен возвращать статус-код 404"
+    )
+
+
+@pytest.mark.usefixtures("charity_project_first", "charity_project_second")
+async def test_delete_project(async_client, charity_project_first):
+    url = os.path.join(URL, "1")
+    response = await async_client.delete(url)
+    assert response.status_code == 204, (
+        f"DELETE-запрос к {url} должен возвращать статус-код 204"
+    )
